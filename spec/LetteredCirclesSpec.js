6d80321811fbd1a90eec6bc686fee3f8cdb6b276
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-10-01 19:51:34 +0100

    Project Set-up

---
+++ spec/LetteredCirclesSpec.js
@@ -0,0 +1,10 @@
+describe("LetteredCircles", function() {
+  beforeEach(function(){
+    jasmine.getFixtures().fixturesPath = '.';
+    loadFixtures('index.html');
+    $.holdReady(false);
+  });
+
+  it('is ready to be tested', function(){
+  });
+});
