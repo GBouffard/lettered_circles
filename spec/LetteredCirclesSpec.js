befd647f53228662bbb10478bc90a889f8f9ef28
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-10-01 20:25:55 +0100

    Written tests (by passed for now)

--- spec/LetteredCirclesSpec.js
+++ spec/LetteredCirclesSpec.js
@@ -1,10 +1,35 @@
-describe("LetteredCircles", function() {
+describe('LetteredCircles', function() {
   beforeEach(function(){
     jasmine.getFixtures().fixturesPath = '.';
     loadFixtures('index.html');
     $.holdReady(false);
   });
 
-  it('is ready to be tested', function(){
+  describe('Stage 1: Make circles', function() {
+    xit('has no initial mouse pointer position when it opens', function(){
+    });
+
+    xit('has no circles already positionned on the page when it opens', function(){
+    });
+
+    xit('knows and shows the X and Y coordinates of the mouse pointer when clicked', function(){
+    });
+
+    xit('triggers the circle drawing method wherever a user clicks on the canvas', function(){
+    });
+  });
+
+  describe('Stage 2: Make lettered circles', function() {
+    xit('has no text in the typeable area when it opens', function(){
+    });
+
+    xit('recognizes when a text has been entered in the typeable area', function(){
+    });
+
+    xit('outputs the first character from the typeable area in a circle wherever a user clicks on the canvas', function(){
+    });
+
+    xit('removes the first letter from the typeable area when a click on the canvas has happened', function(){
+    });
   });
 });
